7b01ab07496eb77e038ffa7a1886cbe2736b63bb
Polyakov Kirll <2696227@mail.ru>
2024-03-14 13:37:20 +0300

    Second commit Calculater

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         do {
             if (calculatorOperator == 'c') {
+                System.out.println("Здравствуйте");
                 System.out.print("Введите первый операнд:  ");
                 val1 = scanner.nextDouble();
                 System.out.print("Введите оператор:  ");
